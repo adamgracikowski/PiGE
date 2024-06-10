e41169cff97523ddf74da73d3a8f7e9d563c6a6e
Adam <admgrac@gmail.com>
2024-06-10 15:15:41 +0200

    add ValidationRules in ImageLabellingTool in Lab04

---
+++ Lab04/ImageLabellingTool/ImageLabellingTool/ValidationRules/DirectoryExistsValidationRule.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using System.Windows.Controls;
+
+namespace ImageLabellingTool
+{
+    public class DirectoryExistsValidationRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, System.Globalization.CultureInfo cultureInfo)
+        {
+            if (value == null || string.IsNullOrWhiteSpace(value.ToString()))
+                return new ValidationResult(false, "Directory path can't be empty.");
+            else if (!Directory.Exists(value.ToString()))
+                return new ValidationResult(false, "Given directory does not exist.");
+            return ValidationResult.ValidResult;
+        }
+    }
+}
